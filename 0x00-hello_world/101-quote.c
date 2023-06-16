51846d8a54932ce0eba71046bc74347a6155daf3
Saraali24 <saramabrouk.ali@gmail.com>
2023-06-15 19:14:40 -0700

    Hello C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
 #include <unistd.h>
-#include <string.h>
+
 /**
- * main - Entry point
- * Return: 1
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
  */
 int main(void)
 {
-	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\n";
-	write(STDERR_FILENO, str, strlen (str));
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
