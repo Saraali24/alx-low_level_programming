281ebff217b19fc8abb9a8be5ec4103a01c0402d
Saraali24 <saramabrouk.ali@gmail.com>
2023-06-15 17:45:40 -0700

    Hello C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 /**
  * main - Entry point
  * Return: always 0 (Success)
@@ -7,5 +8,5 @@ int main(void)
 {
 	 const char* str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(STDERR_FILENO, str, strlen(str));
-	return (1);
+	return 1;
 }
