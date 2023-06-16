b54485e25dbc6250e42a778d0b210a59c235a1a0
Saraali24 <saramabrouk.ali@gmail.com>
2023-06-15 19:08:14 -0700

    Hello C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	const char *str ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write (STDERR_FILENO, str, strlen (str));
+	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, str, strlen (str));
 	return (1);
 }
