3b8b8ccfe1b9edede0de372f8bd73cadc995240b
saraali24 <saramabrouk.ali@gmail.com>
2023-06-20 18:52:55 -0700

    shell redirections

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,32 +1,44 @@
-include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point, prints the first 50 fibonacci numbers
- *
- * Return: Always 0
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
  */
-int main(void)
+void print_times_table(int n)
 {
-	int counter = 0;
-	unsigned long first = 0;
-	unsigned long second = 1;
-	unsigned long fib;
+	int num, mult, prod;
 
-	while (counter <= 97)
+	if (n >= 0 && n <= 15)
 	{
-		fib = first + second;
-		if (counter < 97)
+		for (num = 0; num <= n; num++)
 		{
-			printf("%lu, ", fib);
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
 		}
-		else
-		{
-			printf("%lu", fib);
-		}
-		first = second;
-		second = fib;
-		counter++;
 	}
-	printf("\n");
-	return (0);
 }
