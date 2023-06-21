f3b17dcbf49b60fa82d6dc33113602da80d1a565
saraali24 <saramabrouk.ali@gmail.com>
2023-06-20 18:07:38 -0700

    shell redirections

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_alphabet - function that print alphabet
+*it prints in lowercase
+*Return: no return
+*/
+void print_alphabet(void)
+{
+int d;
+for (d = 'a'; d <= 'z'; d++)
+{
+_putchar (d);
+}
+_putchar (10);
+}
