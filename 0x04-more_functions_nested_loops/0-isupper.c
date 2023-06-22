073ef74fd897ce2f787069af1cadd39e91d23cdf
saraali24 <saramabrouk.ali@gmail.com>
2023-06-22 15:58:59 -0700

    shell redirections

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,16 @@
-#include <cctype>
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isupper - uppercase letters
+ * @c: character to check
+ * Return: 0 or 1
  */
-int isUppercase(char c) {
-    if (isupper(c)) {
-        return 1;
-    } else {
-        return 0;
-    }
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
 }
