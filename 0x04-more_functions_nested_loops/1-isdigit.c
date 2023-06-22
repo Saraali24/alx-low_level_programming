35414ea4a11b87babc7e4347b3325889fc2f6516
saraali24 <saramabrouk.ali@gmail.com>
2023-06-22 16:10:31 -0700

    shell redirections

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,12 @@
-#include <iostream>
+#include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0
- */
-using namespace std;
-
-int main() {
-
-  // checks if '9' is a digit
-  cout << isdigit('9');
-
-  return 0;
-}
-
-// Output: 1
+* _isdigit - write a function that check for a digit (0 through 9).
+* @c: char to check
+* Return:  0 or 1
+**/
+int _isdigit (int c)
+{
+	if ( c=> '0' && c <= '9' ) 
+		return (1);
+	else 
+		return (0);
