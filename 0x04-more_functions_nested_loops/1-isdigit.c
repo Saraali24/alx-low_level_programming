37f490ee8243e39bbefa66077c320a2c8b45e329
saraali24 <saramabrouk.ali@gmail.com>
2023-06-22 15:48:08 -0700

    shell redirections

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <iostream>
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+using namespace std;
+
+int main() {
+
+  // checks if '9' is a digit
+  cout << isdigit('9');
+
+  return 0;
+}
+
+// Output: 1
