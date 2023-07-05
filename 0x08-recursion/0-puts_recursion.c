1cc310d788debdfc8b1d51d9c96dcfd8cd25242f
saraali24 <saramabrouk.ali@gmail.com>
2023-07-05 15:01:45 -0700

    shell redirections

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string
+ *
+ * @s: parameter is character
+ *
+ * Return: returns (0).
+ */
+
+void _puts_recursion(char *s)
+{
+	 if (*s == '\0') { 
+        putchar('\n');
+        return;
+    }
+    putchar(*s);
+    _puts_recursion(s + 1); 
+}
