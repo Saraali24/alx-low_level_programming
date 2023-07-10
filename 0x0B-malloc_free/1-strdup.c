8b43e4743a87278c6ba64755ea5f644fa7e29f3c
saraali24 <saramabrouk.ali@gmail.com>
2023-07-10 15:30:47 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - function which is a duplicate of the string
+ *
+ * @char: parameter is character
+ *
+ * @str: parameter is string
+ *
+ * return: Returns NULL if str = NULL
+ */
+
+char *_strdup(char *str)
+
