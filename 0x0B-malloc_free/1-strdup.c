b1f43245b2be2bc4ff1a04526ca57b7306b41648
saraali24 <saramabrouk.ali@gmail.com>
2023-07-10 17:08:21 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,15 +15,21 @@
 char *_strdup(char *str)
 {
 char *newstr;
-unsigned int i, j;
-if (str == NULL)
-return (NULL);
-for (i = 0; str[i] != '\0'; i++)
-newstr = (char *)malloc(sizeof(char) * (i + 1));
-if (newstr == NULL)
-return (NULL);
-for (j = 0; j <= i; j++)
-newstr[j] = str[j];
-return (newstr);
-free(str)
+	unsigned int i, j;
+
+	if (str == NULL)
+	return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	newstr = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (newstr == NULL)
+	return (NULL);
+
+	for (j = 0; j <= i; j++)
+	newstr[j] = str[j];
+
+	return (newstr);
 }
