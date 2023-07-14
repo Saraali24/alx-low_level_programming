cc2f674e49cf6a3f41c4ab13dba0a45e118c3bf5
saraali24 <saramabrouk.ali@gmail.com>
2023-07-13 18:01:55 -0700

    shell redirections

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - function that concatenates two strings.
+ *
+ * @s1: parameter is character.
+ *
+ * @s2: parameter is character.
+ *
+ * @n: parameter is interger.
+ *
+ * return: returns to a newly allocated space in memory.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned len1, len2, len3, size, i, j;
+if (s1 == NULL)
+S = "";
+if (s2 == NULL)
+s2 = "";
+len1 = 0;
+while (s1[len1]!= '\0')
+len1++;
+len2 = 0;
+while (s2[len2] != '\0')
+len2++;
+
+
+}
+
