4b61880b8faaeea9c7670672a76205ae4f45bc1f
saraali24 <saramabrouk.ali@gmail.com>
2023-07-13 19:03:43 -0700

    shell redirections

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,17 +17,39 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned len1, len2, len3, size, i, j;
+char *nstr;
 if (s1 == NULL)
 S = "";
 if (s2 == NULL)
 s2 = "";
 len1 = 0;
-while (s1[len1]!= '\0')
+while (s1[len1] != '\0')
 len1++;
 len2 = 0;
 while (s2[len2] != '\0')
 len2++;
-
-
+if (n >= len3)
+len3 = len2;
+else 
+len3 = n;
+size = len1 + len3;
+nstr = malloc ((size * sizeof(char))+ 1);
+if (nstr == NULL)
+return (NULL);
+i = 0;
+while (i < len1)
+{
+nstr[i] = s1[i];
+i++;
 }
-
+j = 0;
+while (j < len3)
+{
+nstr[i] = s2[j];
+i++;
+j++;
+}
+nstr[i] = '\0';
+return (nstr);
+free(nstr);
+}
