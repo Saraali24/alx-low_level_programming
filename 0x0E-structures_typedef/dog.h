c2fe92e8873c89622482836bd0ee21b7d8579644
saraali24 <saramabrouk.ali@gmail.com>
2023-07-17 13:50:47 -0700

    structre task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdio.h>
-#include <stdlib.h>
+
+
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
-void dog(char *name, float age, char *owner)
+void dog(struct dog, char *name, float age, char *owner)
 {
 dog;
 }
