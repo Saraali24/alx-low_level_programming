75a302290bdbfdcbddbec860c8dfbebb2a943266
saraali24 <saramabrouk.ali@gmail.com>
2023-07-19 13:41:29 -0700

    function pointers

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,31 @@
+#ifndef FUNCTION_POINTERS.H
+#define FUNCTION_POINTERS.H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /*FUNCTION_POINTERS.H*/
