5161945da858085feb86395d3783f505baafd983
Saraali24 <saramabrouk.ali@gmail.com>
2023-09-18 17:24:29 -0700

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * add - function that that add
+ * @a: parameter is integer
+ * @b: parameter is integer
+ * Return: add
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * sub - function that that sub
+ * @a: parameter is integer
+ * @b: parameter is integer
+ * Return: sub
+ */
+int sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * mul - function that that mul
+ * @a: parameter is integer
+ * @b: parameter is integer
+ * Return: mul
+ */
+int mul(int a, int b)
+{
+return (a *b);
+}
+/**
+ * div - function that that div
+ * @a: parameter is integer
+ * @b: parameter is integer
+ * Return: div
+ */
+int div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * mod - function that that mod
+ * @a: parameter is integer
+ * @b: parameter is integer
+ * Return: mod
+ */
+int mod(int a, int b)
+{
+return (a % b);
+}
